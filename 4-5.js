df354be62074bef7f2de5945842c45d87a923062
ruvsphysics <jihosk1995@live.com>
2022-03-30 12:26:54 -0700

    done #1

---
+++ 4-5.js
@@ -0,0 +1,17 @@
+// #1
+// Two functions – one object
+// importance: 2
+// Is it possible to create functions A and B so that new A() == new B()?
+
+// function A() { ... }
+// function B() { ... }
+
+// let a = new A;
+// let b = new B;
+
+// alert( a == b ); // true
+// If it is, then provide an example of their code.
+let obj = {}
+function A() {
+    return obj
+}
